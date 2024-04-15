6c781a8e2c79706e9214d87f8db958b545a73b0e
Elias Brunila <brunilaelias@gmail.com>
2024-04-15 15:38:54 +0300

    muutoksia

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,44 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_varastoon_lisaa_liikaa_tavaraa(self):
+        varastossa_tilaa = self.varasto.paljonko_mahtuu()
+
+        self.varasto.lisaa_varastoon(varastossa_tilaa)
+        self.varasto.lisaa_varastoon(1)
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+        
+    def test_varastoon_poista_liikaa_tavaraa(self):
+        varastossa_tilaa = self.varasto.paljonko_mahtuu()
+        self.varasto.lisaa_varastoon(varastossa_tilaa)
+        self.varasto.ota_varastosta(varastossa_tilaa)
+        self.varasto.ota_varastosta(1)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+        
+    def test_ota_varastota_negatiivinen_maara(self):
+        varastossa_tilaa = self.varasto.paljonko_mahtuu()
+        self.varasto.lisaa_varastoon(varastossa_tilaa)
+
+        vastaus = self.varasto.ota_varastosta(-varastossa_tilaa)
+        self.assertEqual(vastaus, 0)
+
+    def test_lisaa_varastoon_negatiivinen(self):
+        varastossa_tilaa = self.varasto.paljonko_mahtuu()
+        self.varasto.lisaa_varastoon(-varastossa_tilaa)
+        self.assertEqual(varastossa_tilaa, varastossa_tilaa)
+
+    def test_luodaan_varasto_negatiivinen_tilavuus(self):
+        varasto = Varasto(-10)
+        self.assertEqual(varasto.tilavuus, 0)
+
+    def test_luodaan_varasto_negatiivinen_saldo(self):
+        varasto = Varasto(10, -1)
+        self.assertEqual(varasto.saldo, 0)
+
+    def test_varasto_str(self):
+        s = str(self.varasto)
+        self.varasto.saldo = 0
+        mahtuu = self.varasto.paljonko_mahtuu()
+        vastaus = "saldo = 0, vielä tilaa " + str(mahtuu)
+        self.assertEqual(s, vastaus)
